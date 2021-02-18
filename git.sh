16175f3e1f6112c4045e0b1512f943b32e50d018
Younes Krifa <younes.krifa@gmail.com>
2021-02-18 13:14:02 +0100

    [OB99] testCommit

--- git.sh
+++ git.sh
@@ -2,14 +2,14 @@
 
 
 functionTest(){
-   echo "$0"
    echo "$1"
    echo "$2"
-   git checkout -b "$0"
-   git push origin "$0"
+   echo "$3"
+   git checkout -b "$1"
+   git push origin "$1"
    git add .
-   git commit -m "[$1] $2"
-   git push --set-upstream origin "$0"
+   git commit -m "[$2] $3"
+   git push --set-upstream origin "$1"
 }
 
-functionTest $0 $1 $2
+functionTest $1 $2 $3
